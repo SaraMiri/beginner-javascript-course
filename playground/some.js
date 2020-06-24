568d429117a4a2ef1fc2d4a8570842d7d821b203
Sky Nes <saramiragaya@gmail.com>
2020-06-24 20:55:54 +0200

    Playing with strings

--- playground/some.js
+++ playground/some.js
@@ -1,3 +1,7 @@
 const p = document.querySelector("p");
 console.log("I am in another file");
+
+const age = 100;
 console.log(p);
+
+// SNOB'N'US = String, number, Object, Null, Boolean, Undefined, Symbol
