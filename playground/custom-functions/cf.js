3a151ae9bd19f9746d23c6f4d16344648e679d28
SaraMiri <saramiragaya@gmail.com>
2020-06-26 12:16:00 +0200

    Updating calculateBill

--- playground/custom-functions/cf.js
+++ playground/custom-functions/cf.js
@@ -1,15 +1,35 @@
 // Creating our first function (function definition)
-function calculateBill(billAmount, taxRate ) {
+function calculateBill(billAmount, taxRate = 0.13, tipRate = 0.15 ) {
   //This is the body of the function
-  console.log(billAmount,taxRate);
-  const total = billAmount * (1+ taxRate);
+  console.log("Running calculate bill!")
+  const total = billAmount + (billAmount * taxRate) + (billAmount * tipRate);
   return total;
 }
 
 // Function call or run
 const saraTotal = 500;
 const saraTaxRate = 0.4;
-const myTotal = calculateBill(saraTotal, saraTaxRate);
+//const myTotal = calculateBill(saraTotal, saraTaxRate);
+const myBill = calculateBill(100, undefined , 0.2) // We use undefined to select the default value of the function
+console.log(myBill)
 
 //console.log(myTotal);
 //console.log(`Your total is $${calculateBill(100, 0.13)}`);
+
+// Another function
+function sayHiTo(firstName) {
+  return `Hello ${firstName}`;
+}
+
+const greeting = sayHiTo("Sara");
+//console.log(greeting)
+
+// More functions
+function doctorize(name) {
+ return `Dr. ${name}`;
+}
+
+function yell(name = "Sara") {
+  return `HEY ${name.toUpperCase()}`;
+ }
+ //console.log(yell(doctorize("Sara")))
