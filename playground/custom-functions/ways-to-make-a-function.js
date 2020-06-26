e6bbd82dfd6f9dfb9354de9502021ba71fe74cfc
SaraMiri <saramiragaya@gmail.com>
2020-06-26 14:45:16 +0200

    WIP: custom functions

---
+++ playground/custom-functions/ways-to-make-a-function.js
@@ -0,0 +1,11 @@
+//function doctorize(firstName) {
+//  return `Dra. ${firstName}`
+//}
+
+//function (firstName) {
+ // return `Dra. ${firstName}`
+//}
+
+//function (firstName) {
+ // return `Dra. ${firstName}`
+//}
