021291573dd7b3cacafbef8aacba0a9c775bbced
Sky Nes <saramiragaya@gmail.com>
2020-06-28 14:37:46 +0200

    Arrow functions

--- playground/custom-functions/ways-to-make-a-function.js
+++ playground/custom-functions/ways-to-make-a-function.js
@@ -7,9 +7,32 @@
  // return `Dra. ${firstName}`
 //}
 
+// This is going to fail
+// console.log(doctorize("Sara"))
+
+//This is going to work properly
+console.log(doctorize2("Sara"))
+
 // Function Expression
 const doctorize = function (firstName) {
  return `Dra. ${firstName}`
 }
 
-// Function 4
+function doctorize2(firstName) {
+  return `Dra. ${firstName}`
+}
+
+// Arrow functions
+const inchToCM = inches => inches * 2.54;
+
+const add = (a,b=3) => a +b;
+
+function makeABaby(first,last) {
+  const baby = {
+    name:`${first} ${last}`,
+    age: 0
+  };
+  return baby
+}
+// Returning an object
+const makeABaby2 = (first,last) =>  ({ name: `${first} ${last}`, age: 0 });
