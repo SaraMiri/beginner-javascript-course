d5cddb8a8447d3d3073eb306167198d0e19bbe5e
Sky Nes <saramiragaya@gmail.com>
2020-06-28 15:16:27 +0200

    Callback functions

--- playground/custom-functions/ways-to-make-a-function.js
+++ playground/custom-functions/ways-to-make-a-function.js
@@ -36,3 +36,45 @@ function makeABaby(first,last) {
 }
 // Returning an object
 const makeABaby2 = (first,last) =>  ({ name: `${first} ${last}`, age: 0 });
+
+// IIFE: Inmediately Invoked Function Expression
+(function (age) {
+  return `You are cool and age${age}`
+})(2)
+
+// Methods
+const sara = {
+  name: "Sara MR",
+  // Method
+  sayHi: function() {
+    console.log("Hey Sara");
+    return "Hey Sara"
+  },
+  // Short hand Method
+  yellHi(){
+    console.log("Hey Sara");
+  },
+  // Arrow function
+  whisperHi: () => {
+    console.log("Hi, I'm a dog")
+  }
+}
+
+// Callback Functions
+// Click callback
+const button = document.querySelector(".clickMe");
+
+function handleClick () {
+  console.log("Nice click");
+};
+
+// You can also call a function on the addEventListener (handleClick, for example)
+button.addEventListener("click",function() {
+  console.log("Nice Job!!")
+});
+
+// Timer Callback
+setTimeout(sara.yellHi, 1000)
+setTimeout(function() {
+  console.log("Done! Time to eat.")
+}, 1000);
